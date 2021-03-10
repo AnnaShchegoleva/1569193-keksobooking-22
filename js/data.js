5347e8386be3d22db110878e0ba710afbc9c71e2
AnnaShchegoleva <70628098+AnnaShchegoleva@users.noreply.github.com>
2021-03-10 00:19:52 -0800

    module4-task1

---
+++ js/data.js
@@ -0,0 +1,7 @@
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const bookObjects =[];
+
+export {bookObjects, TYPES, CHECKINS, FEATURES, PHOTOS};
